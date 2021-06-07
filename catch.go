e9661f558a800f875a352f6fb81f5e7cd141ba40
mohammad rendra <abuabdillatief@gmail.com>
2021-06-07 14:52:37 +0700

    refactor + add log info func

--- catch.go
+++ catch.go
@@ -13,6 +13,20 @@ type Catch struct {
 	CatchDirectory string
 }
 
+type PrintType string
+
+const (
+	TypeError PrintType = "Error"
+	TypeWarn  PrintType = "Warn"
+	TypeInfo  PrintType = "Info"
+)
+
+var (
+	yellow = color.New(color.FgYellow).SprintFunc()
+	red    = color.New(color.FgRed).SprintFunc()
+	blue   = color.New(color.FgBlue).SprintFunc()
+)
+
 func NewLog(logFileName string) *Catch {
 	err := os.Remove(logFileName)
 	if err != nil {
@@ -27,80 +41,79 @@ func NewLog(logFileName string) *Catch {
 	}
 }
 
-func Error(e error) {
-	red := color.New(color.FgRed).SprintFunc()
+func DirectoryFormater(printType PrintType) (line int, res string) {
 	_, dir, line, _ := runtime.Caller(0)
 	s := strings.Split(dir, "/")
 	d := s[len(s)-1]
 	s = s[:len(s)-1]
-	s = append(s, color.RedString(d))
+	switch printType {
+	case TypeError:
+		s = append(s, red(d))
+	case TypeWarn:
+		s = append(s, yellow(d))
+	case TypeInfo:
+		s = append(s, blue(d))
+	}
+	res = strings.Join(s, "/")
+	return
+}
 
-	fmt.Println(color.RedString("Error directory  : "), strings.Join(s, "/"))
-	fmt.Printf(`%s at line: %s, message: %s`, red("Error info       : "), color.YellowString(fmt.Sprintf("%d", line)), color.YellowString(e.Error()))
+func Error(e error, m string) {
+	line, dir := DirectoryFormater(TypeError)
+	fmt.Println(red("Error directory  : "), dir)
+	fmt.Printf(`%s at line: %s, message: %s`, red("Error info       : "), yellow(fmt.Sprintf("%d", line)), yellow(m))
+	fmt.Println(red("\nOriginal error   :\n"), red(e.Error()))
 	fmt.Println("\n=================")
 }
 
-func ErrorStr(e string) {
-	red := color.New(color.FgRed).SprintFunc()
-	_, dir, line, _ := runtime.Caller(0)
-	s := strings.Split(dir, "/")
-	d := s[len(s)-1]
-	s = s[:len(s)-1]
-	s = append(s, color.RedString(d))
-
-	fmt.Println(color.RedString("Error directory  : "), strings.Join(s, "/"))
-	fmt.Printf(`%s at line: %s, message: %s`, red("Error info       : "), color.YellowString(fmt.Sprintf("%d", line)), color.YellowString(e))
+func ErrorStr(e string, m string) {
+	line, dir := DirectoryFormater(TypeError)
+	fmt.Println(red("Error directory  : "), dir)
+	fmt.Printf(`%s at line: %s, message: %s`, red("Error info       : "), yellow(fmt.Sprintf("%d", line)), yellow(m))
+	fmt.Println(red("\nOriginal error   :\n"), red(e))
 	fmt.Println("\n=================")
 }
 
-func Warn(e error) {
-	yellow := color.New(color.FgYellow).SprintFunc()
-	_, dir, line, _ := runtime.Caller(0)
-	s := strings.Split(dir, "/")
-	d := s[len(s)-1]
-	s = s[:len(s)-1]
-	s = append(s, color.RedString(d))
-
-	fmt.Println(color.YellowString("Warning directory: "), strings.Join(s, "/"))
-	fmt.Printf(`%s at line: %s, message: %s`, yellow("Warning info     : "), color.YellowString(fmt.Sprintf("%d", line)), color.YellowString(e.Error()))
+func Warn(e error, m string) {
+	line, dir := DirectoryFormater(TypeWarn)
+	fmt.Println(yellow("Warning directory: "), dir)
+	fmt.Printf(`%s at line: %s, message: %s`, yellow("Warning info     : "), yellow(fmt.Sprintf("%d", line)), yellow(m))
+	fmt.Println(yellow("\nOriginal error   :\n"), yellow(e.Error()))
 	fmt.Println("\n=================")
 }
 
-func WarnStr(e string) {
-	yellow := color.New(color.FgYellow).SprintFunc()
-	_, dir, line, _ := runtime.Caller(0)
-	s := strings.Split(dir, "/")
-	d := s[len(s)-1]
-	s = s[:len(s)-1]
-	s = append(s, color.RedString(d))
+func WarnStr(e string, m string) {
+	line, dir := DirectoryFormater(TypeWarn)
+	fmt.Println(yellow("Warning directory: "), dir)
+	fmt.Printf(`%s at line: %s, message: %s`, yellow("Warning info     : "), yellow(fmt.Sprintf("%d", line)), yellow(m))
+	fmt.Println(yellow("\nOriginal error   :\n"), yellow(m))
+	fmt.Println("\n=================")
+}
 
-	fmt.Println(color.YellowString("Warning directory: "), strings.Join(s, "/"))
-	fmt.Printf(`%s at line: %s, message: %s`, yellow("Warning info     : "), color.YellowString(fmt.Sprintf("%d", line)), color.YellowString(e))
+func Inform(e error) {
+	line, dir := DirectoryFormater(TypeInfo)
+	fmt.Println(blue("Current directory: "), dir)
+	fmt.Printf(`%s at line: %s, message: %s`, blue("Info             : "), yellow(fmt.Sprintf("%d", line)), yellow(e.Error()))
+	fmt.Println("\n=================")
+}
+
+func InformStr(e string) {
+	line, dir := DirectoryFormater(TypeInfo)
+	fmt.Println(blue("Current directory: "), dir)
+	fmt.Printf(`%s at line: %s, message: %s`, blue("Info             : "), yellow(fmt.Sprintf("%d", line)), yellow(e))
 	fmt.Println("\n=================")
 }
 
 func (c *Catch) Error(e error) {
-	red := color.New(color.FgRed).SprintFunc()
-	_, dir, line, _ := runtime.Caller(0)
-	s := strings.Split(dir, "/")
-	d := s[len(s)-1]
-	s = s[:len(s)-1]
-	s = append(s, color.RedString(d))
-
-	fmt.Println(color.RedString("Error directory  : "), strings.Join(s, "/"))
-	fmt.Printf(`%s at line: %s, message: %s`, red("Error info       : "), color.YellowString(fmt.Sprintf("%d", line)), color.YellowString(e.Error()))
+	line, dir := DirectoryFormater(TypeError)
+	fmt.Println(red("Error directory  : "), dir)
+	fmt.Printf(`%s at line: %s, message: %s`, red("Error info       : "), yellow(fmt.Sprintf("%d", line)), yellow(e.Error()))
 	fmt.Println("\n=================")
 }
 
 func (c *Catch) ErrorStr(e string) {
-	red := color.New(color.FgRed).SprintFunc()
-	_, dir, line, _ := runtime.Caller(0)
-	s := strings.Split(dir, "/")
-	d := s[len(s)-1]
-	s = s[:len(s)-1]
-	s = append(s, color.RedString(d))
-
-	fmt.Println(color.RedString("Error directory  : "), strings.Join(s, "/"))
-	fmt.Printf(`%s at line: %s, message: %s`, red("Error info       : "), color.YellowString(fmt.Sprintf("%d", line)), color.YellowString(e))
+	line, dir := DirectoryFormater(TypeError)
+	fmt.Println(red("Error directory  : "), dir)
+	fmt.Printf(`%s at line: %s, message: %s`, red("Error info       : "), yellow(fmt.Sprintf("%d", line)), yellow(e))
 	fmt.Println("\n=================")
 }
