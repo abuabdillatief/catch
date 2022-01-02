f0d215009baaf73a2e72a5c6a0db21f32febc8eb
abuabdillatief <abuabdillatief@gmail.com>
2022-01-02 18:20:45 +0700

    change all to .Print

--- main.go
+++ main.go
@@ -2,15 +2,11 @@ package catch
 
 import (
 	"bytes"
-	"errors"
 	"fmt"
 	"log"
-	"net/http"
-	"os"
 	"reflect"
 	"runtime"
 	"strings"
-	"time"
 
 	print "github.com/abuabdillatief/catch/PrintType"
 	"github.com/fatih/color"
@@ -28,11 +24,11 @@ type CatchLogger struct {
 }
 
 var (
-	Yellow = color.New(color.FgYellow).SprintFunc()
-	Red    = color.New(color.FgRed).SprintFunc()
-	Blue   = color.New(color.FgBlue).SprintFunc()
-	Green  = color.New(color.FgGreen).SprintFunc()
-	White  = color.New(color.FgWhite).SprintFunc()
+	Yellow       = color.New(color.FgYellow).SprintFunc()
+	Red          = color.New(color.FgRed).SprintFunc()
+	Blue         = color.New(color.FgBlue).SprintFunc()
+	Green        = color.New(color.FgGreen).SprintFunc()
+	White        = color.New(color.FgWhite).SprintFunc()
 	YellowItalic = color.New(color.FgYellow).Add(color.Italic).SprintFunc()
 	RedItalic    = color.New(color.FgRed).Add(color.Italic).SprintFunc()
 	BlueItalic   = color.New(color.FgBlue).Add(color.Italic).SprintFunc()
@@ -43,7 +39,7 @@ var (
 var C CatchLogger
 
 //====================================================
-func DirectoryFormater(dir string, printType print.PrintType) string {
+func directoryFormater(dir string, printType print.PrintType) string {
 	arr := strings.Split(dir, "/")
 	s := arr[:len(arr)-1]
 	d := arr[len(arr)-1]
@@ -73,23 +69,8 @@ func DirectoryFormater(dir string, printType print.PrintType) string {
 	return strings.Join(s, "/")
 }
 
-// NewLog will create a new CatchLogger instance
-// which can then be used to save logs to a .csv file
-func NewLog() CatchLogger {
-	f, err := os.OpenFile("./catch.log.csv", os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	_, err = f.Write([]byte("date,directory,message"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	return C
-}
-
 // Print will print any value inserted with specified printing style
-func Print(typePrint print.PrintType, values ...interface{}) {
+func _print(typePrint print.PrintType, values ...interface{}) {
 	var B bytes.Buffer
 	clg := log.New(&B, "", log.Llongfile)
 	clg.Output(2, "")
@@ -150,7 +131,7 @@ func Print(typePrint print.PrintType, values ...interface{}) {
 		}
 	}(typePrint)
 
-	log.Println(wd, DirectoryFormater(inf[0], typePrint))
+	log.Println(wd, directoryFormater(inf[0], typePrint))
 	log.Println(fn, White(f[len(f)-1]))
 	log.Printf(`%s at line: %s`, line, Yellow(inf[1]))
 
@@ -165,107 +146,10 @@ func Print(typePrint print.PrintType, values ...interface{}) {
 	}
 }
 
-// PrintArray will print any array of string
-func PrintArrayString(arr []string) {
-	var B bytes.Buffer
-	clg := log.New(&B, "", log.Llongfile)
-	clg.Output(2, "")
-	inf := strings.Split(fmt.Sprintf("%v", &B), ":")
-
-	pc, _, _, _ := runtime.Caller(1)
-	f := strings.Split(runtime.FuncForPC(pc).Name(), ".")
-
-	var wd, fn, line string
-
-	baseWord := "Index"
-	wd = White("Working directory: ")
-	fn = White("Function Name    : ")
-	line = White("Location info    : ")
-
-	log.Println(wd, DirectoryFormater(inf[0], print.TypeNeutral))
-	log.Println(fn, White(f[len(f)-1]))
-	log.Printf(`%s at line: %s`, line, Yellow(inf[1]))
-
-	for i, key := range arr {
-		str := fmt.Sprintf("%s %d", baseWord, i)
-		if len(str) < 17 {
-			str += strings.Repeat(" ", 17-len(str))
-		}
-		str += ": "
-
-		log.Println(White(str), White(key))
-	}
-}
-
-// PrintArrayInt will print any array of string
-func PrintArrayInt(arr []int) {
-	var B bytes.Buffer
-	clg := log.New(&B, "", log.Llongfile)
-	clg.Output(2, "")
-	inf := strings.Split(fmt.Sprintf("%v", &B), ":")
-
-	pc, _, _, _ := runtime.Caller(1)
-	f := strings.Split(runtime.FuncForPC(pc).Name(), ".")
-
-	var wd, fn, line string
-
-	baseWord := "Index"
-	wd = White("Working directory: ")
-	fn = White("Function Name    : ")
-	line = White("Location info    : ")
-
-	log.Println(wd, DirectoryFormater(inf[0], print.TypeNeutral))
-	log.Println(fn, White(f[len(f)-1]))
-	log.Printf(`%s at line: %s`, line, Yellow(inf[1]))
-
-	for i, key := range arr {
-		str := fmt.Sprintf("%s %d", baseWord, i)
-		if len(str) < 17 {
-			str += strings.Repeat(" ", 17-len(str))
-		}
-		str += ": "
-
-		log.Println(White(str), White(key))
-	}
-}
-
-// PrintArrayFloat will print any array of string
-func PrintArrayFloat(arr []float64) {
-	var B bytes.Buffer
-	clg := log.New(&B, "", log.Llongfile)
-	clg.Output(2, "")
-	inf := strings.Split(fmt.Sprintf("%v", &B), ":")
-
-	pc, _, _, _ := runtime.Caller(1)
-	f := strings.Split(runtime.FuncForPC(pc).Name(), ".")
-
-	var wd, fn, line string
-
-	baseWord := "Index"
-	wd = White("Working directory: ")
-	fn = White("Function Name    : ")
-	line = White("Location info    : ")
-
-	log.Println(wd, DirectoryFormater(inf[0], print.TypeNeutral))
-	log.Println(fn, White(f[len(f)-1]))
-	log.Printf(`%s at line: %s`, line, Yellow(inf[1]))
-
-	for i, key := range arr {
-		str := fmt.Sprintf("%s %d", baseWord, i)
-		if len(str) < 17 {
-			str += strings.Repeat(" ", 17-len(str))
-		}
-		str += ": "
-
-		log.Println(White(str), White(key))
-	}
-}
-
-// PrintStruct will print each structs keys and values
-// with specific color type
-func PrintStructWithType(printType print.PrintType, s interface{}) {
+// printStruct will print each structs keys and values
+func printStruct(s interface{}) {
 	if !structs.IsStruct(s) {
-		Print(print.TypeError, "no struct")
+		_print(print.TypeError, "no struct")
 		return
 	}
 	final := reflect.Indirect(reflect.ValueOf(s)).Interface()
@@ -281,35 +165,13 @@ func PrintStructWithType(printType print.PrintType, s interface{}) {
 	clg := log.New(&B, "", log.Llongfile)
 	clg.Output(2, "")
 	inf := strings.Split(fmt.Sprintf("%v", &B), ":")
-	MapPrint(&printType, m, inf, 0)
+	mapPrint(nil, m, inf, 0)
 }
 
-// PrintStruct will print each structs keys and values
-func PrintStruct(s interface{}) {
+// printStruct will print each structs keys and values
+func printStructIndented(s interface{}, indentation int) {
 	if !structs.IsStruct(s) {
-		Print(print.TypeError, "no struct")
-		return
-	}
-	final := reflect.Indirect(reflect.ValueOf(s)).Interface()
-	t := reflect.TypeOf(final)
-	m := make(map[string]interface{})
-	for i := 0; i < t.NumField(); i++ {
-		str := fmt.Sprintf("%c", []byte{t.Field(i).Name[0]})
-		if strings.ToUpper(str) == str {
-			m[t.Field(i).Name] = reflect.ValueOf(final).Field(i).Interface()
-		}
-	}
-	var B bytes.Buffer
-	clg := log.New(&B, "", log.Llongfile)
-	clg.Output(2, "")
-	inf := strings.Split(fmt.Sprintf("%v", &B), ":")
-	MapPrint(nil, m, inf, 0)
-}
-
-// PrintStruct will print each structs keys and values
-func PrintStructIndented(s interface{}, indentation int) {
-	if !structs.IsStruct(s) {
-		Print(print.TypeError, "no struct")
+		_print(print.TypeError, "no struct")
 		return
 	}
 	final := reflect.Indirect(reflect.ValueOf(s)).Interface()
@@ -329,11 +191,11 @@ func PrintStructIndented(s interface{}, indentation int) {
 	clg := log.New(&B, "", log.Llongfile)
 	clg.Output(2, "")
 	inf := strings.Split(fmt.Sprintf("%v", &B), ":")
-	MapPrint(nil, m, inf, indentation)
+	mapPrint(nil, m, inf, indentation)
 }
 
-// MapPrint will print keys and val inside a map
-func MapPrint(printType *print.PrintType, m map[string]interface{}, inf []string, indentation int) {
+// mapPrint will print keys and val inside a map
+func mapPrint(printType *print.PrintType, m map[string]interface{}, inf []string, indentation int) {
 
 	line := inf[1]
 	dir := inf[0]
@@ -368,7 +230,7 @@ func MapPrint(printType *print.PrintType, m map[string]interface{}, inf []string
 		logFunc = func(key1, key2, key3 interface{}, i int) {
 			if i == 0 && indentation == 0 {
 				log.Println("==================")
-				log.Println(White(key3), DirectoryFormater(dir, print.TypeWarn))
+				log.Println(White(key3), directoryFormater(dir, print.TypeWarn))
 				log.Println(White(ei), fmt.Sprintf(`at line: %s`, Yellow(line)))
 				log.Println("==================")
 			}
@@ -385,7 +247,7 @@ func MapPrint(printType *print.PrintType, m map[string]interface{}, inf []string
 				return func(key1, key2, key3 interface{}, i int) {
 					if i == 0 && indentation == 0 {
 						log.Println("==================")
-						log.Println(Red(key3), DirectoryFormater(dir, print.TypeError))
+						log.Println(Red(key3), directoryFormater(dir, print.TypeError))
 						log.Println(Red(ei), fmt.Sprintf(`at line: %s`, Yellow(line)))
 						log.Println("==================")
 					}
@@ -401,7 +263,7 @@ func MapPrint(printType *print.PrintType, m map[string]interface{}, inf []string
 				return func(key1, key2, key3 interface{}, i int) {
 					if i == 0 && indentation == 0 {
 						log.Println("==================")
-						log.Println(Yellow(key3), DirectoryFormater(dir, print.TypeWarn))
+						log.Println(Yellow(key3), directoryFormater(dir, print.TypeWarn))
 						log.Println(Yellow(ei), fmt.Sprintf(`at line: %s`, Yellow(line)))
 						log.Println("==================")
 					}
@@ -417,7 +279,7 @@ func MapPrint(printType *print.PrintType, m map[string]interface{}, inf []string
 				return func(key1, key2, key3 interface{}, i int) {
 					if i == 0 && indentation == 0 {
 						log.Println("==================")
-						log.Println(Blue(key3), DirectoryFormater(dir, print.TypeInfo))
+						log.Println(Blue(key3), directoryFormater(dir, print.TypeInfo))
 						log.Println(Blue(ei), fmt.Sprintf(`at line: %s`, Yellow(line)))
 						log.Println("==================")
 					}
@@ -433,7 +295,7 @@ func MapPrint(printType *print.PrintType, m map[string]interface{}, inf []string
 				return func(key1, key2, key3 interface{}, i int) {
 					if i == 0 && indentation == 0 {
 						log.Println("==================")
-						log.Println(White(key3), DirectoryFormater(dir, print.TypeNeutral))
+						log.Println(White(key3), directoryFormater(dir, print.TypeNeutral))
 						log.Println(White(ei), fmt.Sprintf(`at line: %s`, Yellow(line)))
 						log.Println("==================")
 					}
@@ -449,7 +311,7 @@ func MapPrint(printType *print.PrintType, m map[string]interface{}, inf []string
 				return func(key1, key2, key3 interface{}, i int) {
 					if i == 0 && indentation == 0 {
 						log.Println("==================")
-						log.Println(Green(key3), DirectoryFormater(dir, print.TypeSuccess))
+						log.Println(Green(key3), directoryFormater(dir, print.TypeSuccess))
 						log.Println(Green(ei), fmt.Sprintf(`at line: %s`, Yellow(line)))
 						log.Println("==================")
 					}
@@ -465,7 +327,7 @@ func MapPrint(printType *print.PrintType, m map[string]interface{}, inf []string
 				return func(key1, key2, key3 interface{}, i int) {
 					if i == 0 && indentation == 0 {
 						log.Println("==================")
-						log.Println(Blue(key3), DirectoryFormater(dir, print.TypeInfo))
+						log.Println(Blue(key3), directoryFormater(dir, print.TypeInfo))
 						log.Println(Blue(ei), fmt.Sprintf(`at line: %s`, Yellow(line)))
 						log.Println("==================")
 					}
@@ -486,7 +348,7 @@ func MapPrint(printType *print.PrintType, m map[string]interface{}, inf []string
 		if reflect.ValueOf(val).Kind() == reflect.Struct {
 			logFunc(key, nil, d, i)
 			indentation++
-			PrintStructIndented(val, indentation)
+			printStructIndented(val, indentation)
 		} else {
 			if len(key) != len(cd) {
 				if len(key) > len(cd) {
@@ -508,135 +370,86 @@ func MapPrint(printType *print.PrintType, m map[string]interface{}, inf []string
 	// log.Printf("%s\n", strp)
 }
 
-// SaveToLogfile will save your error message
-// to your catch.log.csv file
-func (c *CatchLogger) SaveToLogFile(e error) {
-	if e == nil {
-		e = errors.New("no error")
-	}
-	f, err := os.OpenFile("./catch.log.csv", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
+func Print(s interface{}) {
+	switch reflect.ValueOf(s).Kind() {
+	case reflect.Slice:
+		arr := reflect.ValueOf(s)
+		baseWord := "Index"
+		cd := "Current directory"
+		ei := "Line info"
 
-	var B bytes.Buffer
-	clg := log.New(&B, "", log.Llongfile)
-	clg.Output(2, "")
-	dir := strings.Split(fmt.Sprintf("%v", &B), "/")
-	d := dir[len(dir)-1]
-	s := d[:len(d)-3]
-
-	t := time.Now().Format(time.RFC3339)
-	_, err = f.Write([]byte(fmt.Sprintf("\n%s,%s,%s", t, s, e.Error())))
-	if err != nil {
-		panic(err)
-	}
-}
-
-// DeleteLogFile will delete your catch.log.csv file
-func (c *CatchLogger) DeleteLogFile() {
-	err := os.Remove("./catch.log.csv")
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err.Error())
-	}
-}
-
-// CustomLog will log every key and value inside catch.Custom
-// after each custom logging, map insde catch.Custom will be deleted
-/*
-	c := catch.NewLog()
-
-	a := map[string]string{
-	"test":"ok",
-	}
-
-	c.Customlog(print.TypeInfo)
-	===================
-	2021/07/12 11:54:57 Current directory:  ~/main.go
-	2021/07/12 11:54:57 Error info       :   at line: 16
-	2021/07/12 11:54:57 tes              :   tes a
-	2021/07/12 11:54:57 foo              :   bar
-*/
-func (c CatchLogger) CustomLog(printType print.PrintType) {
-	if len(c.Custom) == 0 {
-		log.Println("no maps inserted in catch.Custom")
-		return
-	}
-	var B bytes.Buffer
-	clg := log.New(&B, "", log.Llongfile)
-	clg.Output(2, "")
-	inf := strings.Split(fmt.Sprintf("%v", &B), ":")
-	MapPrint(&printType, c.Custom, inf, 0)
-	for key := range c.Custom {
-		delete(c.Custom, key)
-	}
-}
-
-// MiddlewareLogger will log all keys and values inside your HTTP header
-func (c CatchLogger) MiddlewareLogger(createLog bool) func(http.Handler) http.Handler {
-	var B bytes.Buffer
-	clg := log.New(&B, "", log.Llongfile)
-	clg.Output(2, "")
-
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			headers := make(map[string]string)
-			for k, v := range r.Header {
-				headers[k] = v[0]
+		var l string
+		for i := 0; i < arr.Len(); i++ {
+			if len(arr.Index(i).String()) > len(l) {
+				l = arr.Index(i).String()
 			}
-			var l string
-			for key := range headers {
-				if len(key) > len(l) {
-					l = key
-				}
-			}
-			strp := strings.Repeat("_", len(l))
-			log.Println(strp)
-			var i int
-			for key, val := range headers {
-				key += ":  "
-				if i == 0 {
-					i++
-				}
-				log.Println(Blue(key), val)
-			}
-			log.Printf("%s\n", strp)
-			next.ServeHTTP(w, r)
-		})
-	}
-}
+		}
 
-//MiddelwareLoggerWithKeys will log specified keys inside your HTTP header
-func (c CatchLogger) MiddlewareLoggerWithKeys(createLog bool, keys ...string) func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			headers := make(map[string]string)
-			targetKeys := make(map[string]bool)
-			for _, k := range keys {
-				targetKeys[strings.ToLower(k)] = true
-			}
+		if len(l) > len(cd) {
+			cd += strings.Repeat(" ", len(l)-len(cd))
+		} else {
+			l += strings.Repeat(" ", len(cd)-len(l))
+		}
 
-			for k, v := range r.Header {
-				if targetKeys[strings.ToLower(k)] {
-					headers[k] = v[0]
-				}
-			}
-			var l string
-			for key := range headers {
-				if len(key) > len(l) {
-					l = key
-				}
-			}
+		if len(cd) > len(ei) {
+			ei += strings.Repeat(" ", len(cd)-len(ei))
+		}
+		if len(l) > len(ei) {
+			ei += strings.Repeat(" ", len(l)-len(ei))
+		}
+		ei += ":  "
 
-			for key, val := range headers {
-				if len(key) < len(l) {
-					key += strings.Repeat(" ", len(l)-len(key))
-				}
-				key += ":  "
-				log.Println(Blue(key), val)
+		var B bytes.Buffer
+		clg := log.New(&B, "", log.Llongfile)
+		clg.Output(2, "")
+		inf := strings.Split(fmt.Sprintf("%v", &B), ":")
+
+		log.Println("==================")
+		log.Println(White(cd), directoryFormater(inf[0], print.TypeWarn))
+		log.Println(White(ei), fmt.Sprintf(`at line: %s`, Yellow(inf[1])))
+		log.Println("==================")
+		for i := 0; i < arr.Len(); i++ {
+			str := fmt.Sprintf("%s %d", baseWord, i)
+			if len(str) < 17 {
+				str += strings.Repeat(" ", 17-len(str))
 			}
-			next.ServeHTTP(w, r)
-		})
+			str += ": "
+			log.Println(White(str), White(arr.Index(i)))
+		}
+		fmt.Println("")
+	case reflect.Struct:
+		final := reflect.Indirect(reflect.ValueOf(s)).Interface()
+		t := reflect.TypeOf(final)
+		m := make(map[string]interface{})
+		for i := 0; i < t.NumField(); i++ {
+			str := fmt.Sprintf("%c", []byte{t.Field(i).Name[0]})
+			if strings.ToUpper(str) == str {
+				m[t.Field(i).Name] = reflect.ValueOf(final).Field(i).Interface()
+			}
+		}
+		var B bytes.Buffer
+		clg := log.New(&B, "", log.Llongfile)
+		clg.Output(2, "")
+		inf := strings.Split(fmt.Sprintf("%v", &B), ":")
+		mapPrint(nil, m, inf, 0)
+		fmt.Println("")
+	case reflect.Map:
+		newMap := make(map[string]interface{})
+		v := reflect.ValueOf(s)
+		for _, key := range v.MapKeys() {
+			strct := v.MapIndex(key)
+			newMap[key.String()] = strct.Interface()
+		}
+
+		var B bytes.Buffer
+		clg := log.New(&B, "", log.Llongfile)
+		clg.Output(2, "")
+		inf := strings.Split(fmt.Sprintf("%v", &B), ":")
+
+		pt := print.TypeNeutral
+		mapPrint(&pt, newMap, inf, 0)
+		fmt.Println("")
+	default:
+		fmt.Println(reflect.ValueOf(s).Kind())
 	}
 }
